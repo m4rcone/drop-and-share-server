a288d7e8883fff76884ab2239a27834706226917
m4cone <boffmarcone@gmail.com>
2025-08-12 16:10:58 -0300

    feat: create ValidationError and InternalServerError

---
+++ src/infra/errors.ts
@@ -0,0 +1,53 @@
+export class InternalServerError extends Error {
+  action: string;
+  statusCode: number;
+
+  constructor({ cause, statusCode }: { cause: Error; statusCode?: number }) {
+    super("Aconteceu um erro inesperado no servidor.", {
+      cause,
+    });
+    this.name = "InternalServerError";
+    this.action = "Entre em contato com o servidor.";
+    this.statusCode = statusCode || 500;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ValidationError extends Error {
+  action: string;
+  statusCode: number;
+
+  constructor({
+    cause,
+    message,
+    action,
+  }: {
+    cause?: Error;
+    message?: string;
+    action?: string;
+  }) {
+    super(message || "Ocorreu algum erro de validação.", {
+      cause,
+    });
+    this.name = "ValidationError";
+    this.action = action || "Ajuste os dados enviados e tente novamente.";
+    this.statusCode = 400;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
